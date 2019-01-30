ccf7a963eb1f91d1a9c1e10ef590f0b33cbce234
Christopher Black <chris@primeacademy.io>
2019-01-30 14:04:20 -0600

    Added styles for the pre tag to allow for text wrapping.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Garden from './components/Garden/Garden';
 
+import './App.css';
+
 const App = () => (
-  <div>
-    <h1>Welcome to your garden!</h1>
+  <div className="App">
+    <header className="App-header">
+      <h1>Welcome to your garden!</h1>
+    </header>
     <Garden />
   </div>
 );
