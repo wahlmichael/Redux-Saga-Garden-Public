15804ca7a78504ce70aad5aa8fafa27401326aa8
Iris Noh <irisnoh22@gmail.com>
2019-11-06 16:50:11 -0600

    update GET to talk to database

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ import axios from 'axios';
 import App from './App';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
 
-const plantList = (state = startingPlantArray, action) => {
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+      return [ ...state, ...action.payload ]
     default:
       return state;
   }
